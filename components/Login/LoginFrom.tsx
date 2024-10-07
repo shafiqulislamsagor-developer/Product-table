7d7524f86c2f76d7287cf1720256f73152a4b429
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-07 09:17:46 +0600

    fixed name

--- components/Login/LoginFrom.tsx
+++ components/Login/LoginFrom.tsx
@@ -48,7 +48,7 @@ export function LoginForm() {
       <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
         <div className="mb-6 flex items-center rounded-xl bg-black px-5 py-2 text-2xl font-semibold">
           <div className="pl-5 font-clash_display text-2xl font-semibold text-blue-400 lg:pl-0">
-            Dosen<span className="text-white">bd</span>
+            Products<span className="text-white">Shops</span>
           </div>
         </div>
         <div className="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
